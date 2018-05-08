0649bb5d24d26469f167c9b638d4f649c4882d8e
junghwankong <junghwan.kong812@gmail.com>
2018-05-08 12:21:03 +0900

    필요없는 view단 코드 제거

--- app.js
+++ app.js
@@ -9,18 +9,10 @@ var api = require('./routes/api/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/api', api);
 // catch 404 and forward to error handler
