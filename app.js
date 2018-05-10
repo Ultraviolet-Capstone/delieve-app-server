97034853a94174d5787438497987aa030d47e8c8
junghwankong <junghwan.kong812@gmail.com>
2018-05-10 12:43:51 +0900

    error 메세지를 위한 view코드 원복.

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ var api = require('./routes/api/api');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.enable('trust proxy')
 
 app.use('/api', api);
 // catch 404 and forward to error handler
