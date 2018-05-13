faa8d9ca8cb3c53d7f110798b72dc74797e5f061
junghwankong <junghwan.kong812@gmail.com>
2018-05-13 21:58:52 +0900

    chore: lint file

--- routes/api/uv-request/uv-request-query.js
+++ routes/api/uv-request/uv-request-query.js
@@ -1,28 +1,28 @@
 var errorMessage = require('../../../common/error/error-message')
 
 const uvRequestQuery = {
-    insertUvRequest : (res, pool, uvRequest) => {
-        const query = `CALL ADD_UV_REQUEST(
-            ?, ?, ?,
-            ?, ?, ?, 
-            ?,
-            ?, ?,
-            ?, ?, ?, ?
-        )`;
+  insertUvRequest : (res, pool, uvRequest) => {
+    const query = `CALL ADD_UV_REQUEST(
+      ?, ?, ?,
+      ?, ?, ?, 
+      ?,
+      ?, ?,
+      ?, ?, ?, ?
+    )`;
 
-        const parameter = [
-            uvRequest.beginLocation.address, uvRequest.beginLocation.gps.latitude, uvRequest.beginLocation.gps.longitude,
-            uvRequest.finishLocation.address, uvRequest.finishLocation.gps.latitude, uvRequest.finishLocation.gps.longitude,
-            uvRequest.senderId,
-            uvRequest.beginTime, uvRequest.finishTime,
-            uvRequest.stuff.name, uvRequest.stuff.size, uvRequest.stuff.weight, uvRequest.stuff.stuffCode
-        ];
+    const parameter = [
+      uvRequest.beginLocation.address, uvRequest.beginLocation.gps.latitude, uvRequest.beginLocation.gps.longitude,
+      uvRequest.finishLocation.address, uvRequest.finishLocation.gps.latitude, uvRequest.finishLocation.gps.longitude,
+      uvRequest.senderId,
+      uvRequest.beginTime, uvRequest.finishTime,
+      uvRequest.stuff.name, uvRequest.stuff.size, uvRequest.stuff.weight, uvRequest.stuff.stuffCode
+    ];
 
-        if (parameter.includes(undefined)) {
-            return Promise.reject(errorMessage.INSERT_ERROR);
-        } 
-        return pool.query(res, query, parameter)
-    }
+    if (parameter.includes(undefined)) {
+      return Promise.reject(errorMessage.INSERT_ERROR);
+    } 
+    return pool.query(res, query, parameter)
+  }
 }
 
 module.exports = uvRequestQuery;
--- routes/api/uv-request/uv-request-service.js
+++ routes/api/uv-request/uv-request-service.js
@@ -3,9 +3,9 @@ var mysqlPool = require('../../../common/database/mysql');
 mysqlPool.generatePool();
 
 const uvRequestService = {
-    insertUvRequest: function (req, res) {
-        const uvRequest = req.body;
-        return uvRequestQuery.insertUvRequest(res, mysqlPool, uvRequest);
-    }
+  insertUvRequest: function (req, res) {
+    const uvRequest = req.body;
+    return uvRequestQuery.insertUvRequest(res, mysqlPool, uvRequest);
+  }
 }
 module.exports = uvRequestService;
