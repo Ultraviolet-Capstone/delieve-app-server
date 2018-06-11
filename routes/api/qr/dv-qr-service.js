a7513445bb57d81b1d7fb7e0df3b1b2aa1b7a5a4
Dogfooter <dogfooter219@gmail.com>
2018-06-11 23:44:58 +0900

    add: 문자보내는 코드에 개발버전 분기를 넣

--- routes/api/qr/dv-qr-service.js
+++ routes/api/qr/dv-qr-service.js
@@ -43,7 +43,7 @@ const dvQRService = {
       })
       .then(status => {
         returnStatus = status;
-        if (returnStatus == 'PROGRESS') {
+        if (returnStatus == 'PROGRESS' && !process.env.dev) {
           var url = "http://13.125.124.127:4200/qr/" + matchingId;
           phone = phone.replace('-', '').trim();
           return sms.send({
